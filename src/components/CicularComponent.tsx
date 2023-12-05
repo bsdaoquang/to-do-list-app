8fc078f6dc03207c56ef47e652b1e0c5ed04a9ee
bsdaoquang <bsdaoquang@gmail.com>
2023-12-05 09:13:21 +0700

    add progress

--- src/components/CicularComponent.tsx
+++ src/components/CicularComponent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CircularProgress from 'react-native-circular-progress-indicator';
+import {colors} from '../constants/colors';
+import {fontFamilies} from '../constants/fontFamilies';
 
 interface Props {
   color?: string;
@@ -9,7 +11,20 @@ interface Props {
 
 const CicularComponent = (props: Props) => {
   const {color, value, maxValue} = props;
-  return <CircularProgress value={value} />;
+  return (
+    <CircularProgress
+      value={value}
+      title={`${value}%`}
+      showProgressValue={false}
+      activeStrokeColor={color ?? colors.blue}
+      inActiveStrokeColor={'#3C444A'}
+      titleColor={colors.text}
+      titleFontSize={32}
+      titleStyle={{
+        fontFamily: fontFamilies.semiBold,
+      }}
+    />
+  );
 };
 
 export default CicularComponent;
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,7 +1,8 @@
 export const colors = {
   bgColor: '#171719',
   gray: '#222329',
+  gray2: '#696B6F',
   text: '#ffffff',
   desc: '#e6e6e6',
-  blue: '#3618e0',
+  blue: '#3618e0b3',
 };
